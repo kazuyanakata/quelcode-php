cbef40fa66ca5979395cc975a6f4dbb0d045f7e2
kazuyanakata <68310528+kazuyanakata@users.noreply.github.com>
2020-08-17 21:42:28 +0900

    曖昧な比較から厳密な比較に変更。（それに伴う型の変更含む）

--- html/php-pre-challenge3/index.php
+++ html/php-pre-challenge3/index.php
@@ -23,11 +23,11 @@ if (is_numeric($limit) && $limit >= 1 && !preg_match("/^0/",$limit) && !preg_mat
   for($j=0;$j < 2**8-1 ;$j++){
     $str[$j] = str_split(sprintf('%08d',decbin($j + 1)));
     for($k=0;$k <= 7;$k++){
-      if($str[$j][$k] == 1){
+      if((int)$str[$j][$k] === 1){
         $sum[$j][] = $num[$k];
       } 
     }
-    if(array_sum($sum[$j]) == $limit){
+    if(array_sum($sum[$j]) === (int)$limit){
       $array[] = $sum[$j];
     }
   }
