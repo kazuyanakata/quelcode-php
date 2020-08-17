6f2a22db6655b381d44efbecadc2cb7e9d72ac08
kazuyanakata <68310528+kazuyanakata@users.noreply.github.com>
2020-08-17 17:16:56 +0900

    PHP練習課題2　初回実装

--- html/php-pre-challenge2/index.php
+++ html/php-pre-challenge2/index.php
@@ -2,11 +2,17 @@
 $array = explode(',', $_GET['array']);
 
 // 修正はここから
-for ($i = 0; $i < count($array); $i++) {
-
+$count = count($array);
+for ($i = 0; $i < $count; $i++) {
+  $min = min($array);
+  $subArray[$i] = $min;
+  $key = array_search($min , $array);
+  unset($array[$key]);
 }
+$array = $subArray;
 // 修正はここまで
 
 echo "<pre>";
 print_r($array);
 echo "</pre>";
+?>
